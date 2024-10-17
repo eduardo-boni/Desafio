8a308d67608e2c318c6fe855d62922a188c851f3
Eduardo Motta Ishimura <eduardoishimura@gmail.com>
2024-10-17 17:06:03 -0300

    Desafio de projeto resolvido

---
+++ src/index.js
@@ -0,0 +1,31 @@
+
+function resultadoDeJogo(quantDeVitoria, quantDeDerrota) {
+    
+let saldo = quantDeVitoria - quantDeDerrota;
+let nivel = "";
+    switch (true) {
+        case (saldo < 10):
+            nivel = "Ferro";
+            break;
+        case (saldo >= 11) && (saldo <= 20):
+            nivel = "Bronze";
+            break;
+        case (saldo >= 21) && (saldo <= 50):
+            nivel = "Prata";
+            break;
+        case (saldo >= 51) && (saldo <= 80):
+            nivel = "Ouro";
+            break;
+        case (saldo >= 81) && (saldo <= 90):
+            nivel = "Platina";
+            break;
+        case (saldo > 91) && (saldo < 100):
+            nivel = "Ascendente"; 
+            break;
+        case saldo >= 101:
+            nivel = "Imortal"; 
+            break;
+        }
+        return `O Herói tem de saldo de ${saldo} está no nível de ${nivel}`
+}
+console.log(resultadoDeJogo(76, 25))
